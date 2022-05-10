be57cff57c5c5b965011affe431ec8b8a9371fb5
chao <a0921342997@gmail.com>
2022-05-11 05:30:19 +0800

    Resizing all glasses texture imgaes and fixed someError

--- src/utils/useFrameScene.js
+++ src/utils/useFrameScene.js
@@ -4,18 +4,19 @@ import { useFrame } from "@react-three/fiber";
 
 const useFrameScene = ({ view3d, group, glasses }) => {
   useFrame(({ camera }) => {
-    if (!view3d) return;
+    if (view3d) {
+      group.current.position.copy(camera.position);
+      group.current.rotation.copy(camera.rotation);
 
-    group.current.position.copy(camera.position);
-    group.current.rotation.copy(camera.rotation);
-
-    if (camera.rotation._y < 0) {
-      glasses.current.material.opacity = 0.4;
-      glasses.current.material.alphaTest = 0.3;
-    } else {
-      glasses.current.material.opacity = 0.2;
-      glasses.current.material.alphaTest = 0.1;
+      if (camera.rotation._y < 0) {
+        glasses.current.material.opacity = 0.4;
+        glasses.current.material.alphaTest = 0.3;
+      } else {
+        glasses.current.material.opacity = 0.2;
+        glasses.current.material.alphaTest = 0.1;
+      }
     }
+    return;
   });
 };
 

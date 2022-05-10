f3b2b86a337ac7edaf7ba7da022c6409ea7fc19c
chao <a0921342997@gmail.com>
2022-05-11 07:41:59 +0800

    Change Aviator tortoiseshell glasses model

--- src/threeModel/aviator/aviator-tortoiseshell.js
+++ src/threeModel/aviator/aviator-tortoiseshell.js
@@ -16,75 +16,75 @@ const Glasses = ({ nodes, materials, props, glasses }) => (
     />
     <mesh
       geometry={nodes.Object_4.geometry}
-      material={materials.Aluminum_Brushed_90X}
+      material={materials.Clear_Shiny_Plastic_Orange}
     />
     <mesh
       geometry={nodes.Object_5.geometry}
-      material={materials.Paint_Gloss_Purple}
+      material={materials.Aluminum_Brushed_90X}
     />
     <mesh
       geometry={nodes.Object_6.geometry}
       material={materials.Paint_Textured_Yellow_1}
     />
-    <mesh
-      geometry={nodes.Object_7.geometry}
-      material={materials.Paint_Textured_Green}
-    />
+    <mesh geometry={nodes.Object_7.geometry} material={materials.Rubber} />
     <mesh
       geometry={nodes.Object_8.geometry}
-      material={materials.Paint_Metallic_White}
+      material={materials.Paint_Gloss_Purple}
     />
     <mesh
       geometry={nodes.Object_9.geometry}
-      material={materials["footleft2ptc-ceramic255255242"]}
+      material={materials.Paint_Metallic_White}
     />
     <mesh
       geometry={nodes.Object_10.geometry}
-      material={materials["footright3ptc-ceramic255255242"]}
+      material={materials["footleft2ptc-ceramic255255242"]}
     />
     <mesh
       geometry={nodes.Object_11.geometry}
-      material={materials.Clear_Shiny_Plastic_Orange}
+      material={materials.material_20}
     />
     <mesh
       geometry={nodes.Object_12.geometry}
-      material={materials.material_19}
+      material={materials["footright3ptc-ceramic255255242"]}
     />
     <mesh geometry={nodes.Object_13.geometry} material={materials.material} />
     <mesh
       geometry={nodes.Object_14.geometry}
-      material={materials.material_17}
+      material={materials.material_21}
     />
-    <mesh geometry={nodes.Object_15.geometry} material={materials.Rubber} />
+    <mesh
+      geometry={nodes.Object_15.geometry}
+      material={materials.Paint_Textured_Green}
+    />
     <mesh
       geometry={nodes.Object_16.geometry}
-      material={materials.material_22}
+      material={materials.material_18}
     />
     <mesh
       geometry={nodes.Object_17.geometry}
+      material={materials.material_19}
+    />
+    <mesh
+      geometry={nodes.Object_18.geometry}
+      material={materials.material_17}
+    />
+    <mesh
+      geometry={nodes.Object_19.geometry}
       material={materials.material_23}
       material-opacity={props.mode}
       ref={glasses}
     />
     <mesh
-      geometry={nodes.Object_18.geometry}
-      material={materials.material_20}
-    />
-    <mesh
-      geometry={nodes.Object_19.geometry}
-      material={materials.material_21}
-    />
-    <mesh
       geometry={nodes.Object_20.geometry}
-      material={materials.material_18}
+      material={materials.material_22}
     />
     <mesh
       geometry={nodes.Object_21.geometry}
-      material={materials.Paint_Matte_Green}
+      material={materials.Paint_Gloss_Red}
     />
     <mesh
       geometry={nodes.Object_22.geometry}
-      material={materials.Paint_Gloss_Red}
+      material={materials.Paint_Matte_Green}
     />
     <mesh
       geometry={nodes.Object_23.geometry}

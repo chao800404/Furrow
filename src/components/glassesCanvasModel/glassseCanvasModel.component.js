a36565fdc0154ba6840eda1f22595dadd82933cb
chao <a0921342997@gmail.com>
2022-05-15 16:53:48 +0800

    Fixed glasses model

--- src/components/glassesCanvasModel/glassseCanvasModel.component.js
+++ src/components/glassesCanvasModel/glassseCanvasModel.component.js
@@ -26,47 +26,48 @@ const GlassesModel = ({ type, color, toggleElectrochromic, transitionEnd }) => {
   const CurGlassesModel = glassesModel[curType][curColor];
 
   const set = () => setFir((prev) => (prev = true));
-  console.log(fir);
 
   useTimeOut(set, 100);
 
   return (
     <ClassesModelContainer>
-      <Canvas shadows camera={{ position: [0, 20, 0], fov: 35 }} dpr={[1, 2]}>
-        <ambientLight intensity={0.2} />
-        <spotLight
-          intensity={0.5}
-          angle={0.1}
-          penumbra={1}
-          position={[10, 15, 10]}
-          castShadow
-        />
-        <Suspense fallback={null}>
-          {transitionEnd && fir && (
-            <CurGlassesModel
-              onPointerDown={() => dispatch(checkARIsPointer())}
-              mode={
-                toggleElectrochromic ? (curType === "marki" ? 2.5 : 1) : 0.3
-              }
-            />
-          )}
-          <ContactShadows
-            rotation-x={Math.PI / 2}
-            position={[0, -2.5, 0]}
-            opacity={0.7}
-            width={30}
-            height={30}
-            blur={1.5}
-            far={4.5}
+      {fir && (
+        <Canvas shadows camera={{ position: [0, 20, 0], fov: 35 }} dpr={[1, 2]}>
+          <ambientLight intensity={0.2} />
+          <spotLight
+            intensity={0.5}
+            angle={0.1}
+            penumbra={1}
+            position={[10, 15, 10]}
+            castShadow
           />
-          <Environment preset="sunset" />
-        </Suspense>
-        <OrbitControls
-          minPolarAngle={Math.PI / 2}
-          maxPolarAngle={Math.PI / 3}
-          enablePan={false}
-        />
-      </Canvas>
+          <Suspense fallback={null}>
+            {transitionEnd && (
+              <CurGlassesModel
+                onPointerDown={() => dispatch(checkARIsPointer())}
+                mode={
+                  toggleElectrochromic ? (curType === "marki" ? 2.5 : 1) : 0.3
+                }
+              />
+            )}
+            <ContactShadows
+              rotation-x={Math.PI / 2}
+              position={[0, -2.5, 0]}
+              opacity={0.7}
+              width={30}
+              height={30}
+              blur={1.5}
+              far={4.5}
+            />
+            <Environment preset="sunset" />
+          </Suspense>
+          <OrbitControls
+            minPolarAngle={Math.PI / 2}
+            maxPolarAngle={Math.PI / 3}
+            enablePan={false}
+          />
+        </Canvas>
+      )}
 
       {pointDown ? null : (
         <SvgIcon

569f5af8cc067b50420864581c12f32a28b80620
chao <a0921342997@gmail.com>
2022-05-31 18:31:53 +0800

    Fixed glasses Model 5/31 -12

--- src/components/glassesCanvasModel/glassseCanvasModel.component.js
+++ src/components/glassesCanvasModel/glassseCanvasModel.component.js
@@ -7,7 +7,7 @@ import {
   Loader,
   Html,
 } from "@react-three/drei";
-import { Suspense, useMemo, useRef, useEffect, useState } from "react";
+import { Suspense, useMemo, useRef, useEffect } from "react";
 import { SvgIcon, ClassesModelContainer } from "./glassesCanvasModel.styles";
 import { ReactSVG } from "react-svg";
 import svg from "../../assets/svgIcon/AR-icon.svg";
@@ -21,7 +21,6 @@ import { glassesModel } from "./glassesCanvasToMaps";
 const GlassesModel = ({ type, color, toggleElectrochromic }) => {
   const dispatch = useDispatch();
   const pointDown = useSelector(selectCardIsPointer);
-  const [firstLoad, setFirstLoad] = useState(false);
 
   const { curType, curColor } = transferClassesTypeName({ type, color });
   const canvasElem = useRef(null);
@@ -30,21 +29,6 @@ const GlassesModel = ({ type, color, toggleElectrochromic }) => {
     () => glassesModel[curType][curColor],
     [curColor, curType]
   );
-
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      setFirstLoad((prev) => (prev = true));
-    }, 1000);
-    const reload = setTimeout(() => {
-      setFirstLoad((prev) => (prev = false));
-    }, 1000);
-    return () => {
-      clearTimeout(timeOut);
-      clearTimeout(reload);
-    };
-  }, []);
-
-  console.log(firstLoad);
 
   useEffect(() => {
     const onPointDown = () => dispatch(checkARIsPointer());
@@ -57,71 +41,69 @@ const GlassesModel = ({ type, color, toggleElectrochromic }) => {
   return (
     <ClassesModelContainer ref={canvasElem}>
       <>
-        {!firstLoad && (
-          <Canvas
-            shadows
-            camera={{ position: [0, 20, 0], fov: 40 }}
-            dpr={[1, 2]}
-            resize={{ scroll: false }}
-          >
-            <ambientLight intensity={0.2} />
-            <spotLight
-              intensity={0.5}
-              angle={0.1}
-              penumbra={1}
-              position={[10, 15, 10]}
-              castShadow
+        <Canvas
+          shadows
+          camera={{ position: [0, 20, 0], fov: 40 }}
+          dpr={[1, 2]}
+          resize={{ scroll: false }}
+        >
+          <ambientLight intensity={0.2} />
+          <spotLight
+            intensity={0.5}
+            angle={0.1}
+            penumbra={1}
+            position={[10, 15, 10]}
+            castShadow
+          />
+          <Suspense fallback={null}>
+            <CurGlassesModel
+              mode={
+                toggleElectrochromic ? (curType === "marki" ? 2.5 : 1) : 0.3
+              }
             />
-            <Suspense fallback={null}>
-              <CurGlassesModel
-                mode={
-                  toggleElectrochromic ? (curType === "marki" ? 2.5 : 1) : 0.3
-                }
-              />
 
-              <Html as="div" wrapperClass="vr">
-                {!pointDown && (
-                  <SvgIcon
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                      repeat: Infinity,
-                      duration: 2,
-                      ease: "easeInOut",
-                    }}
-                  >
-                    <ReactSVG
-                      src={svg}
-                      fontSize="1rem"
-                      beforeInjection={(svg) =>
-                        svg.setAttribute(
-                          "style",
-                          "display:flex;align-items:center;justify-content:center;width:3.5rem;height:3.5rem"
-                        )
-                      }
-                    />
-                  </SvgIcon>
-                )}
-              </Html>
-              <ContactShadows
-                rotation-x={Math.PI / 2}
-                position={[0, -2.5, 0]}
-                opacity={0.7}
-                width={30}
-                height={30}
-                blur={1.5}
-                far={4.5}
-              />
-              <Environment preset="sunset" />
-            </Suspense>
-            <OrbitControls
-              minPolarAngle={Math.PI / 2}
-              maxPolarAngle={Math.PI / 3}
-              enablePan={false}
-              resize={{ scroll: false }}
+            <Html as="div" wrapperClass="vr">
+              {!pointDown && (
+                <SvgIcon
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{
+                    repeat: Infinity,
+                    duration: 2,
+                    ease: "easeInOut",
+                  }}
+                >
+                  <ReactSVG
+                    src={svg}
+                    fontSize="1rem"
+                    beforeInjection={(svg) =>
+                      svg.setAttribute(
+                        "style",
+                        "display:flex;align-items:center;justify-content:center;width:3.5rem;height:3.5rem"
+                      )
+                    }
+                  />
+                </SvgIcon>
+              )}
+            </Html>
+            <ContactShadows
+              rotation-x={Math.PI / 2}
+              position={[0, -2.5, 0]}
+              opacity={0.7}
+              width={30}
+              height={30}
+              blur={1.5}
+              far={4.5}
             />
-          </Canvas>
-        )}
+            <Environment preset="sunset" />
+          </Suspense>
+          <OrbitControls
+            minPolarAngle={Math.PI / 2}
+            maxPolarAngle={Math.PI / 3}
+            enablePan={false}
+            resize={{ scroll: false }}
+          />
+        </Canvas>
 
         <Loader />
       </>
--- src/page/home-Page/hompage.component.js
+++ src/page/home-Page/hompage.component.js
@@ -4,6 +4,7 @@ import { lazy } from "react";
 import HomeContent from "../../components/home/homeContent/homeContent.component";
 import HomeFeature from "../../components/home/homeFeature/homeFeature.component";
 import HomeArticle from "../../components/home/homArticle/homeArticle.component";
+import GlassesModel from "../../components/glassesCanvasModel/glassseCanvasModel.component";
 const HomeBanner = lazy(() =>
   import("../../components/home/homeBanner/homeBanner.component")
 );
@@ -15,6 +16,7 @@ const Homepage = () => {
       <HomeContent />
       <HomeFeature />
       <HomeArticle />
+      <GlassesModel type="marki" color="black" />
     </>
   );
 };

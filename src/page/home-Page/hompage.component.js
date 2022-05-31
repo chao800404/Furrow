c157b282c6f122e378613f112d81578f1d41279b
chao <a0921342997@gmail.com>
2022-05-31 18:54:49 +0800

    Fixed glasses Model 5/31 -14

--- src/page/home-Page/hompage.component.js
+++ src/page/home-Page/hompage.component.js
@@ -1,15 +1,27 @@
 /** @format */
 
-import { lazy } from "react";
+import { lazy, useState, useEffect } from "react";
 import HomeContent from "../../components/home/homeContent/homeContent.component";
 import HomeFeature from "../../components/home/homeFeature/homeFeature.component";
 import HomeArticle from "../../components/home/homArticle/homeArticle.component";
 import GlassesModel from "../../components/glassesCanvasModel/glassseCanvasModel.component";
+import isMoible from "../../utils/useCheckScreen";
 const HomeBanner = lazy(() =>
   import("../../components/home/homeBanner/homeBanner.component")
 );
 
 const Homepage = () => {
+  const [firstLoad, setFirstLoad] = useState(false);
+
+  useEffect(() => {
+    if (!firstLoad) {
+      const timeOut = setTimeout(() => {
+        setFirstLoad((prev) => (prev = true));
+      }, 300);
+      return clearTimeout(timeOut);
+    }
+  }, [firstLoad]);
+
   return (
     <>
       <HomeBanner />
@@ -17,7 +29,13 @@ const Homepage = () => {
       <HomeFeature />
       <HomeArticle />
 
-      <GlassesModel style={{ display: "none" }} type="marki" color="black" />
+      {isMoible && firstLoad && (
+        <GlassesModel
+          style={{ width: "10rem", height: "10rem" }}
+          type="marki"
+          color="black"
+        />
+      )}
     </>
   );
 };

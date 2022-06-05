d55a2017a3ea20c3ce10ebcf13b579e0a0152100
chao <a0921342997@gmail.com>
2022-06-06 01:21:59 +0800

    Testing preload glasses model -3

--- src/page/home-Page/hompage.component.js
+++ src/page/home-Page/hompage.component.js
@@ -1,48 +1,48 @@
 /** @format */
 
-// import { lazy, useCallback, useState } from "react";
-import { lazy } from "react";
-// import useCheckScreenIsMobile from "../../utils/useCheckScreen";
+import { lazy, useCallback, useState } from "react";
+
+import useCheckScreenIsMobile from "../../utils/useCheckScreen";
 import HomeContent from "../../components/home/homeContent/homeContent.component";
 import HomeFeature from "../../components/home/homeFeature/homeFeature.component";
 import HomeArticle from "../../components/home/homArticle/homeArticle.component";
 
-// import GlassesModel from "../../components/glassesCanvasModel/glassseCanvasModel.component";
+import GlassesModel from "../../components/glassesCanvasModel/glassseCanvasModel.component";
 
 const HomeBanner = lazy(() =>
   import("../../components/home/homeBanner/homeBanner.component")
 );
 
 const Homepage = () => {
-  // const [firstLoad, setFirstLoad] = useState(true);
-  // const isMobile = useCheckScreenIsMobile();
-  // const callbackUnMount = useCallback(() => {
-  //   const timeOut = setTimeout(() => {
-  //     setFirstLoad((prev) => (prev = false));
-  //   }, 100);
-  //   return () => clearTimeout(timeOut);
-  // }, []);
+  const [firstLoad, setFirstLoad] = useState(true);
+  const isMobile = useCheckScreenIsMobile();
+  const callbackUnMount = useCallback(() => {
+    const timeOut = setTimeout(() => {
+      setFirstLoad((prev) => (prev = false));
+    }, 100);
+    return () => clearTimeout(timeOut);
+  }, []);
 
-  // const PreLoadGlasses = () => {
-  //   // Glasses model in ios mobile have bug, need to preload glasses model once
-  //   return (
-  //     isMobile &&
-  //     firstLoad && (
-  //       <GlassesModel
-  //         style={{
-  //           position: "absolute",
-  //           bottom: "0",
-  //           width: "100%",
-  //           height: "10rem",
-  //           right: "500%",
-  //         }}
-  //         type="marki"
-  //         color="black"
-  //         onUpdate={callbackUnMount}
-  //       />
-  //     )
-  //   );
-  // };
+  const PreLoadGlasses = () => {
+    // Glasses model in ios mobile have bug, need to preload glasses model once
+    return (
+      isMobile &&
+      firstLoad && (
+        <GlassesModel
+          style={{
+            position: "absolute",
+            bottom: "0",
+            width: "100%",
+            height: "15rem",
+            right: "1000000%",
+          }}
+          type="marki"
+          color="black"
+          onUpdate={callbackUnMount}
+        />
+      )
+    );
+  };
 
   return (
     <>
@@ -51,7 +51,7 @@ const Homepage = () => {
       <HomeFeature />
       <HomeArticle />
 
-      {/* <PreLoadGlasses /> */}
+      <PreLoadGlasses />
     </>
   );
 };
